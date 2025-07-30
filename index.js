998e72d63425cb765499c404f14f6df5be913409
Darren <darrenyong99@gmail.com>
2025-07-30 22:20:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const express = require('express');
-const fs = require('fs');
-const app = express();
 
-let latestQR = null;
+const app = express();
+const port = 3000;
 
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
-      "--disable-gpu"
-    ],
-    executablePath: '/usr/bin/chromium' // Point to system Chromium
+    args: ['--no-sandbox']
   }
 });
 
-client.on('qr', qr => {
-  latestQR = qr;
+client.on('qr', (qr) => {
+  console.log('QR RECEIVED', qr);
   qrcode.generate(qr, { small: true });
 });
 
@@ -35,14 +24,10 @@ client.on('ready', () => {
 
 client.initialize();
 
-app.get('/qr', (req, res) => {
-  if (latestQR) {
-    res.send(`<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${latestQR}" />`);
-  } else {
-    res.send('QR code not generated yet. Please wait...');
-  }
+app.get('/', (req, res) => {
+  res.send('WhatsApp Bot is running!');
 });
 
-app.listen(3000, () => {
-  console.log('QR server running at http://localhost:3000');
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
